6fe35a208c7d806643b22618e95f06e6ebd320e2
xitie2000 <45730455+xitie2000@users.noreply.github.com>
2022-08-30 12:22:20 +0800

    update sfo09

---
+++ java/sfo_09_implement_a_queue_using_two_stacks_m1/sfo_09_implement_a_queue_using_two_stacks_m1.java
@@ -0,0 +1,47 @@
+/*
+* File: sfo_09_implement_a_queue_using_two_stacks_m1.java
+* Date: 2022-08-30
+* Author: xitie2000
+* Url: https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+*/
+
+package sfo_09_implement_a_queue_using_two_stacks_m1;
+
+import include.*;
+import java.util.*;
+
+// ===== Solution Code =====
+class CQueue {
+    LinkedList<Integer> st1, st2;
+    public CQueue() {
+        st1 = new LinkedList<>();
+        st2 = new LinkedList<>();
+    }
+    
+    public void appendTail(int value) {
+        st1.offerLast(value);
+    }
+    
+    public int deleteHead() {
+        if (!st2.isEmpty()) return st2.pollLast();
+        if (st1.isEmpty()) return -1;
+        while (!st1.isEmpty()) {
+            st2.offerLast(st1.pollLast());
+        }
+        return st2.pollLast();
+    }
+}
+
+public class sfo_09_implement_a_queue_using_two_stacks_s1 {
+    public static void main(String[] args) {
+        // ====== Driver Code ======
+        List<Integer> res = new ArrayList<>();
+        CQueue cQueue = new CQueue();
+        res.add(cQueue.deleteHead());
+        cQueue.appendTail(5);
+        cQueue.appendTail(2);
+        res.add(cQueue.deleteHead());
+        res.add(cQueue.deleteHead());
+        System.out.println(Arrays.toString(res.toArray()));
+    }
+}
